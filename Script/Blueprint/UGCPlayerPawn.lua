4a41dc899981e542191b9dd298bbb6eceb6bf143
WIN-KALLUKN8IRB\Administrator <481982877@qq.com>
2026-07-14 16:32:58 +0800

    Initial project upload

---
+++ Script/Blueprint/UGCPlayerPawn.lua
@@ -0,0 +1,32 @@
+local UGCPlayerPawn = {}
+ 
+--[[
+function UGCPlayerPawn:ReceiveBeginPlay()
+    UGCPlayerPawn.SuperClass.ReceiveBeginPlay(self)
+end
+--]]
+
+--[[
+function UGCPlayerPawn:ReceiveTick(DeltaTime)
+    UGCPlayerPawn.SuperClass.ReceiveTick(self, DeltaTime)
+end
+--]]
+
+--[[
+function UGCPlayerPawn:ReceiveEndPlay()
+    UGCPlayerPawn.SuperClass.ReceiveEndPlay(self) 
+end
+--]]
+
+--[[
+function UGCPlayerPawn:GetAvailableServerRPCs()
+    return
+end
+--]]
+
+function UGCPlayerPawn:GetReplicatedProperties()
+    return {"__SubObjectRepList", "Lazy"}
+end
+
+
+return UGCPlayerPawn
