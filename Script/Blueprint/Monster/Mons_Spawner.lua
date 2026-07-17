ae63a118c11f97a9c7c4e9768dd08e23293c46b7
doudouqie66 <doudouqie66@gmail.com>
2026-07-17 15:23:18 +0800

    提交初始模型及其相关寻路配置

---
+++ Script/Blueprint/Monster/Mons_Spawner.lua
@@ -0,0 +1,24 @@
+---@class Mons_Spawner_C:BP_UGCMobSpawner_C
+--Edit Below--
+local Mons_Spawner = {}
+ 
+--[[
+function Mons_Spawner:ReceiveBeginPlay()
+    Mons_Spawner.SuperClass.ReceiveBeginPlay(self)
+end
+--]]
+
+--[[
+function Mons_Spawner:OnMobSpawn(MobPawn)
+    
+end
+--]]
+
+--[[
+function Mons_Spawner:CustomSpawnMob(InCustomParam)
+    
+end
+--]]
+
+
+return Mons_Spawner
---
+++ Script/Blueprint/Prefabs/Monsters/BaseMonse_Child.lua
@@ -0,0 +1,116 @@
+---@class BaseMonse_Child_C:BaseMonse_C
+--Edit Below--
+local BaseMonse_Child = {}
+
+-- function BaseMonse_Child:ReceiveBeginPlay()
+--     BaseMonse_Child.SuperClass.ReceiveBeginPlay(self)
+-- end
+
+-- function BaseMonse_Child:ReceiveTick(DeltaTime)
+--     BaseMonse_Child.SuperClass.ReceiveTick(self, DeltaTime)
+-- end
+
+-- function BaseMonse_Child:ReceiveEndPlay()
+--     BaseMonse_Child.SuperClass.ReceiveEndPlay(self) 
+-- end
+
+-- function BaseMonse_Child:GetReplicatedProperties()
+--     return
+-- end
+
+-- ---受击前置事件
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- function BaseMonse_Child:PreTakeDamageEvent(Damage, EventInstigator, DamageCauser, DamageContext)
+     
+-- end
+
+-- ---受击后置事件
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- function BaseMonse_Child:PostTakeDamageEvent(Damage, EventInstigator, DamageCauser, DamageContext)
+    
+-- end
+
+-- ---受击前置伤害修改
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- ---@return float 修改后的伤害值
+-- function BaseMonse_Child:PreOverrideDamage(Damage, EventInstigator, DamageCauser, DamageContext)
+--     return Damage
+-- end
+
+-- ---受击后置伤害修改
+-- ---生效范围：服务器
+-- ---@param Damage float 伤害值
+-- ---@param EventInstigator AController 伤害来源的Controller
+-- ---@param DamageCauser AActor 伤害来源
+-- ---@param DamageContext FGameMagnitudeContext  伤害上下文
+-- ---@return float 修改后的伤害值
+-- function BaseMonse_Child:PostOverrideDamage(Damage, EventInstigator, DamageCauser, DamageContext)
+--     return Damage
+-- end
+
+---角色死亡事件
+---生效范围：服务器&客户端
+---@param Damage float 伤害值
+---@param EventInstigator AController 伤害来源的Controller
+---@param DamageCauser AActor 伤害来源
+---@param FDamageEvent DamageEvent 伤害事件
+---@param DamageTypeID int32 伤害类型
+function BaseMonse_Child:BPDie(KillingDamage, EventInstigator, DamageCauser, DamageEvent, DamageTypeID)
+    if self:HasAuthority() then
+        -- 只有服务端才可以掉落
+        self.UGCPresetCommonDropItemComponent:StartDrop(self, EventInstigator, {})
+    end
+end
+
+-- ---状态进入事件
+-- ---生效范围：服务器&客户端
+-- ---@param DynamicState FGameplayTag 进入的状态
+-- function BaseMonse_Child:OnEnterTagState_BP(DynamicState)
+--     local Tag = BlueprintGameplayTagLibrary.GetTagName(DynamicState)
+--     ugcprint('OnEnterTagState_BP: ' .. Tag)
+-- end
+
+-- ---状态退出事件
+-- ---生效范围：服务器&客户端
+-- ---@param DynamicState FGameplayTag 退出的状态
+-- function BaseMonse_Child:OnLeaveTagState_BP(DynamicState)
+--     local Tag = BlueprintGameplayTagLibrary.GetTagName(DynamicState)
+--     ugcprint('OnLeaveTagState_BP: ' .. Tag)
+-- end
+
+-- ---状态打断事件
+-- ---生效范围：服务器&客户端
+-- ---@param DynamicState FGameplayTag 打断的状态
+-- function BaseMonse_Child:OnInterruptTagState_BP(DynamicState)
+--     local Tag = BlueprintGameplayTagLibrary.GetTagName(DynamicState)
+--     ugcprint('OnInterruptTagState_BP' .. Tag)
+-- end
+
+-- ---行为树消息
+-- ---生效范围：服务器
+-- ---@param NotifyMsg string 消息
+-- function BaseMonse_Child:OnBehaviorNotify_BP(NotifyMsg)
+--     ugcprint('OnBehaviorNotify_BP: ' .. NotifyMsg)
+-- end
+
+-- ---怪物的目标发生变化事件
+-- ---生效范围：服务器&客户端
+-- ---@param NewTarget AActor 新目标
+-- ---@param OldTarget AActor 旧目标
+-- function BaseMonse_Child:OnTargetChange_BP(NewTarget, OldTarget)
+    
+-- end
+
+return BaseMonse_Child
