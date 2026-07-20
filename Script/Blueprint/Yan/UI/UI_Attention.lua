4dcb726e14ea297463bce5ebf84478c1d5ec0d1c
sy <3231283647@qq.com>
2026-07-20 16:41:31 +0800

    添加提示的UI

---
+++ Script/Blueprint/Yan/UI/UI_Attention.lua
@@ -0,0 +1,20 @@
+---@class UI_Attention_C:UUserWidget
+---@field Image_35 UImage
+--Edit Below--
+local UI_Attention = { bInitDoOnce = false } 
+
+--[==[ Construct
+function UI_Attention:Construct()
+	
+end
+-- Construct ]==]
+
+-- function UI_Attention:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function UI_Attention:Destruct()
+
+-- end
+
+return UI_Attention
