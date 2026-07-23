6ee11f3063716d710024a06befd15898db165fd5
朋 <doudouqie66@gmail.com>
2026-07-23 18:21:29 +0800

    实现礼包购买即使用

--- Script/Blueprint/Yan/UI/UI03.lua
+++ Script/Blueprint/Yan/UI/UI03.lua
@@ -50,10 +50,17 @@
 ---@field Image_360 UImage
 ---@field Image_481 UImage
 -- Edit Below--
+UGCGameSystem.UGCRequire("ExtendResource.GiftPack.OfficialPackage.Script.GiftPack.GiftPackManager")
+
+local Gift_Pack_Product_ID = 9000029 -- 礼包对应的商品ID
+local Gift_Pack_ID = 1030 -- 礼包表中的礼包ID
+
 local UI03 = {
-    bInitDoOnce = false
+    bInitDoOnce = false,
+    Pending_Open_Gift_Pack = false
 }
 
+--[[----------------------构造商城界面------------------------]]
 function UI03:Construct()
     self:LuaInit();
 
@@ -63,11 +70,14 @@ end
 
 -- end
 
--- function UI03:Destruct()
-
--- end
+--[[----------------------销毁商城界面并解绑委托------------------------]]
+function UI03:Destruct()
+    UGCCommoditySystem.BuyUGCCommodityResultDelegate:Remove(self.OnBuyUGCCommodityResult, self)
+    self.Virtual_Item_Manager.OnItemNumUpdatedDelegate:Remove(self.OnItemNumUpdated, self)
+end
 
 -- [Editor Generated Lua] function define Begin:
+--[[----------------------初始化商城界面------------------------]]
 function UI03:LuaInit()
     if self.bInitDoOnce then
         return;
@@ -79,25 +89,41 @@ function UI03:LuaInit()
     -- [Editor Generated Lua] BindingEvent Begin:
     self.Button_330.OnClicked:Add(self.Button_330_OnClicked, self);
     self.Button_108.OnClicked:Add(self.Button_108_OnClicked, self);
+    UGCCommoditySystem.BuyUGCCommodityResultDelegate:Add(self.OnBuyUGCCommodityResult, self)
+    self.Virtual_Item_Manager = GiftPackManager:GetVirtualItemManager() -- 获取虚拟物品管理器
+    self.Virtual_Item_Manager.OnItemNumUpdatedDelegate:Add(self.OnItemNumUpdated, self)
     -- [Editor Generated Lua] BindingEvent End;
 end
 
+--[[----------------------关闭商城界面------------------------]]
 function UI03:Button_330_OnClicked()
     L_GloTools.UIMgr(L_Enum.Name_ClassPath.UI03, false)
 
 end
---[[----------------------购买按键------------------------]] --
+
+--[[----------------------购买礼包商品------------------------]]
 function UI03:Button_108_OnClicked()
 
-    local Product_ID = 9000029 -- 商品表中的商品ID
     local Buy_Count = 1 -- 购买数量
-    local Product_Data = ShopV2Manager:GetProductConfigData(Product_ID) -- 商品信息
+    local Product_Data = ShopV2Manager:GetProductConfigData(Gift_Pack_Product_ID) -- 商品信息
     local Object_Data = ShopV2Manager:GetItemConfigData(Product_Data.ItemID) -- 物品信息
 
-    local Purchase_Future = UGCCommoditySystem.BuyUGCCommodity2(Product_ID, Object_Data.ItemIcon, Object_Data.ItemDesc,
-        Buy_Count)
-    -- UGCCommoditySystem.BuyUGCCommodity2(9000029, nil, nil, 1)
+    UGCCommoditySystem.BuyUGCCommodity2(Gift_Pack_Product_ID, Object_Data.ItemIcon, Object_Data.ItemDesc, Buy_Count)
+end
 
+--[[----------------------记录购买成功后待开启的礼包------------------------]]
+function UI03:OnBuyUGCCommodityResult(bSuccess, PlayerKey, CommodityID, Count, UID, ProductID)
+    if bSuccess and ProductID == Gift_Pack_Product_ID and CommodityID == Gift_Pack_ID then
+        self.Pending_Open_Gift_Pack = true
+    end
+end
+
+--[[----------------------礼包到账后自动开启------------------------]]
+function UI03:OnItemNumUpdated()
+    if self.Pending_Open_Gift_Pack and GiftPackManager:GetItemNum(Gift_Pack_ID) > 0 then
+        self.Pending_Open_Gift_Pack = false
+        GiftPackManager:OpenGiftPack(Gift_Pack_ID)
+    end
 end
 
 -- [Editor Generated Lua] function define End;
