c0bd2616496507e3c98c887d7728a99d9a8f6e1a
朋 <doudouqie66@gmail.com>
2026-07-24 14:59:19 +0800

    实现使用

--- Script/Blueprint/Prefabs/Items/Item_CanUse_Tmp_Child_25.lua
+++ Script/Blueprint/Prefabs/Items/Item_CanUse_Tmp_Child_25.lua
@@ -1,8 +1,19 @@
 ---@class Item_CanUse_Tmp_Child_25_C:Item_CanUse_Tmp_C
---Edit Below--
-local Item_CanUse_Tmp_Child_25 = {} 
+-- Edit Below--
+---@class Item_CanUse_Tmp_Child_25_C:Item_CanUse_Tmp_C
+-- Edit Below--
+local Item_CanUse_Tmp_Child_25 = {}
 
---[[经典背包事件]]--
+--[[----------------------向使用物品的玩家显示房间密码------------------------]]
+function Item_CanUse_Tmp_Child_25:OnUseV2()
+    Item_CanUse_Tmp_Child_25.SuperClass.OnUseV2(self)
+
+    local Own_Backpack_Component = UGCItemSystemV2.GetOwnBackpackComponent(self) -- 获取所属背包组件
+    local Player_Controller = Own_Backpack_Component:GetOwner() -- 获取使用物品的玩家
+    local Pass = UGCGameSystem.GameState.Room_Pass -- 获取房间密码
+    L_TipsTool.ShowTips_01("密码是" .. tostring(Pass), Player_Controller)
+end
+--[[经典背包事件]] --
 --[[
 --- func 处理物品的拾取(服务端生效)
 ---@return bool @是否拾取该物品, 返回true才能拾取进背包
@@ -50,9 +61,9 @@ local Item_CanUse_Tmp_Child_25 = {}
 -- function Item_CanUse_Tmp_Child_25:HanldeCleared()
 --    return Item_CanUse_Tmp_Child_25.SuperClass.HanldeCleared(self)
 -- end
-]]--
+]] --
 
---[[V2背包事件]]--
+--[[V2背包事件]] --
 --[[
 --- func 能否创建物品Handle(服务端生效)
 ---@return bool @是否允许创建物品Handle, 若不允许，物品也将创建失败
@@ -116,6 +127,6 @@ local Item_CanUse_Tmp_Child_25 = {}
 -- function Item_CanUse_Tmp_Child_25:UGC_OnStopUse(Reason)
     Item_CanUse_Tmp_Child_25.SuperClass.UGC_OnStopUse(self, Reason)
 -- end
-]]--
+]] --
 
 return Item_CanUse_Tmp_Child_25
