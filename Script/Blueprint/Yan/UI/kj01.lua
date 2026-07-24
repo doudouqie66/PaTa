ca2c13b6597baca1d624c8141c3ca43048834871
sy <3231283647@qq.com>
2026-07-24 19:19:43 +0800

    修改UI

---
+++ Script/Blueprint/Yan/UI/kj01.lua
@@ -0,0 +1,22 @@
+---@class kj01_C:UUserWidget
+---@field Button_88 UButton
+---@field Image_16 UImage
+---@field Image_18 UImage
+--Edit Below--
+local kj01 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function kj01:Construct()
+	
+end
+-- Construct ]==]
+
+-- function kj01:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function kj01:Destruct()
+
+-- end
+
+return kj01
