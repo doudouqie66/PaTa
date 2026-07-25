ec14a402247457a5a4513972c6a90f3ab97f0bda
朋 <doudouqie66@gmail.com>
2026-07-25 17:48:57 +0800

    修复黑夜

--- Script/Blueprint/UGCPlayerController.lua
+++ Script/Blueprint/UGCPlayerController.lua
@@ -64,9 +64,9 @@ function UGCPlayerController:InitTest()
         if self:HasAuthority() == true then
             local PlayerPawn = self:GetPlayerCharacterSafety()
             -- V2 背包添加物品
-            UGCBackpackSystemV2.AddItemV2(PlayerPawn, 8310033, 1)
-            UGCBackpackSystemV2.AddItemV2(PlayerPawn, 8310035, 1)
-            UGCBackpackSystemV2.AddItemV2(PlayerPawn, 8310036, 1)
+            -- UGCBackpackSystemV2.AddItemV2(PlayerPawn, 8310033, 1)
+            -- UGCBackpackSystemV2.AddItemV2(PlayerPawn, 8310035, 1)
+            -- UGCBackpackSystemV2.AddItemV2(PlayerPawn, 8310036, 1)
 
         end
     end)
