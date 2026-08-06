932974390ce4ea3f4872bb9584ab29a405fbb300
sy <3231283647@qq.com>
2026-08-06 12:53:21 +0800

    抽奖UI

---
+++ Script/Blueprint/Yan/UI/UI13.lua
@@ -0,0 +1,29 @@
+---@class UI13_C:UUserWidget
+---@field Button_69 UButton
+---@field Button_112 UButton
+---@field Button_147 UButton
+---@field Button_194 UButton
+---@field Image_0 UImage
+---@field Image_70 UImage
+---@field Image_95 UImage
+---@field Image_145 UImage
+---@field Image_170 UImage
+---@field Image_265 UImage
+--Edit Below--
+local UI13 = { bInitDoOnce = false } 
+
+--[==[ Construct
+function UI13:Construct()
+	
+end
+-- Construct ]==]
+
+-- function UI13:Tick(MyGeometry, InDeltaTime)
+
+-- end
+
+-- function UI13:Destruct()
+
+-- end
+
+return UI13
