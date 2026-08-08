9c6131aefbe163680e300b122cbf098c459f9cfb
朋 <doudouqie66@gmail.com>
2026-08-08 17:44:41 +0800

    缝缝补补

--- Script/Blueprint/Yan/UI/UI11.lua
+++ Script/Blueprint/Yan/UI/UI11.lua
@@ -1,14 +1,20 @@
 ---@class UI11_C:UUserWidget
+---@field Gold_Move_UP UWidgetAnimation
+---@field Move_Down UWidgetAnimation
+---@field Box_Before UWidgetAnimation
 ---@field Rotate_Anim UWidgetAnimation
 ---@field Button_72 UButton
 ---@field Button_147 UButton
+---@field CanvasPanel_2 UCanvasPanel
 ---@field CanvasPanel_74 UCanvasPanel
+---@field Image_0 UImage
 ---@field Image_96 UImage
 ---@field Image_285 UImage
 ---@field Image_286 UImage
 ---@field TextBlock_48 UTextBlock
 ---@field TextBlock_49 UTextBlock
 ---@field UIParticleEmitter_11 UUIParticleEmitter
+---@field UIParticleEmitter_22 UUIParticleEmitter
 --Edit Below--
 local UI11 = {
     bInitDoOnce = false
@@ -34,6 +40,8 @@ function UI11:LuaInit()
     end
     self.bInitDoOnce = true;
     self:PlayAnimation(self.Rotate_Anim, 0, 0, EUMGSequencePlayMode.Forward, 0.1);
+    self:PlayAnimation(self.Box_Before, 0, 0, EUMGSequencePlayMode.Forward, 0.1);
+
     -- [Editor Generated Lua] BindingProperty Begin:
     -- [Editor Generated Lua] BindingProperty End;
 
@@ -51,21 +59,49 @@ function UI11:Button_147_OnClicked()
 end
 --[[----------------------开金币------------------------]] --
 function UI11:Button_72_OnClicked()
+    self.Button_72:SetIsEnabled(false)
 
-    local Drop_Result = UGCDropSystem.DropItems(4)
-
+    local Drop_Result = UGCDropSystem.DropItems(5)
     local Drop_Count = Drop_Result[1005]
-    self.CanvasPanel_74:SetVisibility(ESlateVisibility.Visible)
-    self.TextBlock_49:SetText("金币+" .. tostring(Drop_Count))
-    self.Button_72:SetVisibility(ESlateVisibility.Collapsed)
-    self.TextBlock_48:SetVisibility(ESlateVisibility.Collapsed)
-    self.Image_96:SetVisibility(ESlateVisibility.Collapsed)
-    local PC = UGCGameSystem.GetLocalPlayerController()
+    ---触发点击图片
+    self.Image_0:SetVisibility(ESlateVisibility.Visible)
 
+    ---先往下移动
+    self:PlayAnimation(self.Move_Down, 0, 1, EUMGSequencePlayMode.Forward, 1);
+
+    ---
     UGCTimerUtility.CreateLuaTimer(1, function()
-        UnrealNetwork.CallUnrealRPC(PC, PC, L_Enum.Name_RPC.Grant_Virtual_Item, 1005, Drop_Count)
-    end)
+        ---打开宝箱开启的UI
+        self.CanvasPanel_74:SetVisibility(ESlateVisibility.Visible)
+        self.CanvasPanel_2:SetVisibility(ESlateVisibility.Collapsed)
 
+        ---播放金币向上的动画
+        self:PlayAnimation(self.Gold_Move_UP, 0, 1, EUMGSequencePlayMode.Forward, 0.5);
+        UGCTimerUtility.CreateLuaTimer(2, function()
+            ---关闭点击图片
+
+            self.Image_0:SetVisibility(ESlateVisibility.Collapsed)
+
+            ---文本打开
+            self.TextBlock_49:SetVisibility(ESlateVisibility.Visible)
+            ---开启跳动的动画
+            local Gold_Count_Tween = UGCTweenSystem.TweenFloatValue(0, Drop_Count, 3, EEasingType.QuadOut,
+                function(_, Gold_Count)
+                    self.TextBlock_49:SetText("+" .. tostring(math.floor(Gold_Count + 0.5)))
+                end, UGCTweenSystem.MakeConfig(0, 0, false, 0))
+
+            UGCTweenSystem.BindCompletedDelegate(Gold_Count_Tween, function()
+                self.TextBlock_49:SetText("+" .. tostring(Drop_Count))
+            end)
+
+            ---获得奖励
+            UGCTimerUtility.CreateLuaTimer(3, function()
+                local PC = UGCGameSystem.GetLocalPlayerController()
+                UnrealNetwork.CallUnrealRPC(PC, PC, L_Enum.Name_RPC.Grant_Virtual_Item, 1005, Drop_Count)
+
+            end)
+        end)
+    end)
 end
 
 -- [Editor Generated Lua] function define End;
