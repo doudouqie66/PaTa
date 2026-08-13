d9e16e52edae84032a7c057e2d04f16976766f5f
sy <3231283647@qq.com>
2026-08-13 19:24:22 +0800

    1

--- Script/Blueprint/Yan/UI/UI03.lua
+++ Script/Blueprint/Yan/UI/UI03.lua
@@ -26,6 +26,35 @@
 ---@field TextBlock_434 UTextBlock
 ---@field TextBlock_435 UTextBlock
 ---@field TextBlock_436 UTextBlock
+--Edit Below--
+---@class UI03_C:UUserWidget
+---@field Button_107 UButton
+---@field Button_108 UButton
+---@field Button_203 UButton
+---@field Button_204 UButton
+---@field Button_205 UButton
+---@field Button_206 UButton
+---@field Button_207 UButton
+---@field Button_208 UButton
+---@field Button_209 UButton
+---@field Button_210 UButton
+---@field Button_330 UButton
+---@field Image_55 UImage
+---@field Image_56 UImage
+---@field Image_57 UImage
+---@field Image_216 UImage
+---@field Image_217 UImage
+---@field Image_218 UImage
+---@field Image_219 UImage
+---@field Image_220 UImage
+---@field Image_221 UImage
+---@field Image_222 UImage
+---@field Image_223 UImage
+---@field Image_360 UImage
+---@field Image_481 UImage
+---@field TextBlock_434 UTextBlock
+---@field TextBlock_435 UTextBlock
+---@field TextBlock_436 UTextBlock
 -- Edit Below--
 ---@class UI03_C:UUserWidget
 ---@field Button_107 UButton
--- Script/Blueprint/Yan/UI/UI08.lua
+++ Script/Blueprint/Yan/UI/UI08.lua
@@ -10,6 +10,19 @@
 ---@field Image_209 UImage
 ---@field Image_210 UImage
 ---@field Image_286 UImage
+--Edit Below--
+---@class UI08_C:UUserWidget
+---@field Button_88 UButton
+---@field Button_159 UButton
+---@field EditorUtilityEditableTextBox_216 UEditorUtilityEditableTextBox
+---@field Image_0 UImage
+---@field Image_16 UImage
+---@field Image_18 UImage
+---@field Image_34 UImage
+---@field Image_97 UImage
+---@field Image_209 UImage
+---@field Image_210 UImage
+---@field Image_286 UImage
 -- Edit Below--
 local UI08 = {
     bInitDoOnce = false
