47e2fc89cc9058e1ae1e8db2d110e3aac5c81830
朋 <doudouqie66@gmail.com>
2026-08-19 12:59:37 +0800

    11

--- Script/Blueprint/Prefabs/Regions/Aire01.lua
+++ Script/Blueprint/Prefabs/Regions/Aire01.lua
@@ -9,11 +9,23 @@
 -- Edit Below--
 local Aire01 = {}
 
---[[
+--[[----------------------绑定香蕉皮盒体重叠事件------------------------]]
 function Aire01:ReceiveBeginPlay()
     Aire01.SuperClass.ReceiveBeginPlay(self)
+    if UGCGameSystem.IsServer() then
+        self.Box.OnComponentBeginOverlap:Add(self.Box_OnComponentBeginOverlap, self)
+    end
 end
---]]
+
+--[[----------------------玩家踩中香蕉皮时播放音效------------------------]]
+function Aire01:Box_OnComponentBeginOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep,
+    SweepResult)
+    local Player_Controller = UGCGameSystem.GetPlayerControllerByPlayerPawn(OtherActor) -- 踩中香蕉皮的玩家控制器
+    if Player_Controller then
+        UnrealNetwork.CallUnrealRPC(Player_Controller, Player_Controller, L_Enum.Name_RPC.Play_Sound,
+            SoundMgr.SoundName.Banana)
+    end
+end
 
 --[[
 function Aire01:ReceiveTick(DeltaTime)
