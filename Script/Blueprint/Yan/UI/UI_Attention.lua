df44d5a0f57f906378711344730f525ec5165539
朋 <doudouqie66@gmail.com>
2026-08-21 17:17:13 +0800

    1

--- Script/Blueprint/Yan/UI/UI_Attention.lua
+++ Script/Blueprint/Yan/UI/UI_Attention.lua
@@ -1,6 +1,7 @@
 ---@class UI_Attention_C:UUserWidget
 ---@field wh UWidgetAnimation
 ---@field Image_35 UImage
+---@field Image_Danger_Overlay UImage
 --Edit Below--
 local UI_Attention = { bInitDoOnce = false } 
 
