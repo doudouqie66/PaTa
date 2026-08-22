adbc7804e3c5e5757fd9ceb39b4441f9c018b156
朋 <doudouqie66@gmail.com>
2026-08-22 16:06:30 +0800

    1

--- Script/L_Com/L_GloTools.lua
+++ Script/L_Com/L_GloTools.lua
@@ -14,7 +14,7 @@ function L_GloTools.UIMgr(str, bVisible, Is_Change_SysUI) -- 是否修改系统U
         local UI_Class = UE.LoadClass(str);
         local PlayerController = UGCGameSystem.GetLocalPlayerController()
         UI_BP = UserWidget.NewWidgetObjectBP(PlayerController, UI_Class);
-        if str == L_Enum.Name_ClassPath.UI01 then
+        if str == L_Enum.Name_ClassPath.UI01 or str == L_Enum.Name_ClassPath.UI_Attention then
             UI_BP:AddToViewport(9999999);
 
         else
